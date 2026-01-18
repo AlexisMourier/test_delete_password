8d3cc6fcf455f060f8b1f21df69b8efeb0387e60
Alexis Mourier <alexis.mourier@grenoble-inp.org>
2026-01-18 15:11:30 +0100

    Quatrième commit sain

--- Makefile
+++ Makefile
@@ -1,1 +1,3 @@
 password = monmotdepasse
+
+nouveaux trucs qu'on ajoute comme ça
--- main.py
+++ main.py
@@ -4,12 +4,17 @@ def main():
 def main2():
     for i in range(10):
         print(i)
+    return i
 
 def main3():
     i = 0
     while (i < 10):
         print(i)
         i+=1
+    return i
+
+def main4():
+    return main2() + main3()
 
 if __name__ == "__main__":
     main()
